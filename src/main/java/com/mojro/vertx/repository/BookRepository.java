d6f67088a37a3644949c74fbb75f33a6fbe16b33
Venkatesh Gudi <vggudi@gmail.com>
2020-04-27 12:09:46 +0530

    RESTful WebService using Spring and Vertx

--- src/main/java/com/mojro/vertx/repository/BookRepository.java
+++ src/main/java/com/mojro/vertx/repository/BookRepository.java
@@ -3,5 +3,5 @@ package com.mojro.vertx.repository;
 import com.mojro.vertx.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, String> {
 }
